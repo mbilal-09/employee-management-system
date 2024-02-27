d36889bd5d71d89b1f377da06d7049b8e97e50e6
mbilal-09 <bilalkhanshakir@gmail.com>
2024-02-28 02:54:51 +0500

    email provider added

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,12 +1,12 @@
 // pages/api/auth/[...nextauth].js
 
 import NextAuth from 'next-auth';
-import Providers from 'next-auth/providers';
+import Credentials from 'next-auth/providers/credentials';
 
-export default NextAuth({
+const handler = NextAuth({
   // Configure one or more authentication providers
   providers: [
-    Providers.Credentials({
+    Credentials({
       // The name to display on the sign-in form (e.g., 'Sign in with...')
       name: 'Credentials',
       credentials: {
@@ -16,12 +16,18 @@ export default NextAuth({
       },
       authorize: async (credentials) => {
         // Add logic to validate credentials, usually by querying your database
-        const user = { id: 1, name: 'John Doe', email: 'john@example.com' };
 
-        if (user) {
-          return Promise.resolve(user);
+        const { username, password } = credentials;
+
+        if (username === 'bilal@gmail.com' && password === 'password') {
+          // If credentials are valid, return the user object
+
+          console.log('username---->' + username, password);
+
+          return { email: username };
+
         } else {
-          return Promise.resolve(null);
+          return false
         }
       }
     })
@@ -30,13 +36,18 @@ export default NextAuth({
   callbacks: {
     async signIn(user, account, profile) {
       // Custom sign-in logic if needed
-      console.log(user, account, profile);
+      console.log("user------>"+ user);
       return true;
     },
-    async session(session, user) {
-      // Add properties to the session object
-      session.user.id = user.id;
+    async session({ session }) {
+      console.log(  'session-->', session);
+      // const sessionUser = await User.findOne({
+      //   email: session.user.email,
+      // });
+      // session.user.id = sessionUser._id.toString();
+
       return session;
-    }
-  }
+  }}
 });
+
+export { handler as GET, handler as POST };
