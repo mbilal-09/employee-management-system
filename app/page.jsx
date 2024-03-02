06158ab8a61ed49a3e53fc961ad684a0f774f545
M Bilal <121584126+mbilal-09@users.noreply.github.com>
2024-03-03 01:55:03 +0500

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -7,7 +7,7 @@ import Button from "@/components/ui/Button";
 export default function Home() {
   const router = useRouter();
   const { data: session } = useSession();
-
+console.log(session)
 
   if (session?.data?._id) {
     if (session.data?.type === "admin") {
